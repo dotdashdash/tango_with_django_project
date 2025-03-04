7024e8bd3211e21251080aa902ab29553629fb41
Shuhan <wangsh24252@outlook.com>
2025-03-04 15:52:23 +0000

    add tags, checklist, and notes fields to Task model; update serializers and views to handle new fields

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"timehero","static"),  # 指定 `static` 目录
 ]
-STATIC_ROOT=os.path.join(BASE_DIR,"timehero","staticfiles")
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
